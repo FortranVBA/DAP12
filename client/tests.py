654d1b2f7c669b699eb913c5b788eab3b38ae33d
FortranVBA <mpyfabrice@gmail.com>
2021-09-01 15:32:36 +0200

    Ajouts des tests et permissions client

--- client/tests.py
+++ client/tests.py
@@ -19,19 +19,259 @@ class ClientTests(APITestCase):
         """
         Test a successful authentification.
         """
+        url = reverse('login')
+        data = {'username': 'StaffManagementA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
         url = reverse('clients-list')
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
 
     def test_retrieve_client_as_management(self):
-        """
-        Test a successful authentification.
-        """
+        url = reverse('login')
+        data = {'username': 'StaffManagementA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
         url = reverse('clients-detail', kwargs = {'pk': 1})
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["CompanyName"], "ClientA")
 
+    def test_list_client_as_sales(self):
+        url = reverse('login')
+        data = {'username': 'StaffSalesA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
 
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+    def test_list_client_as_support(self):
+        url = reverse('login')
+        data = {'username': 'StaffSupportA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 3)
+
+    def test_list_client_with_fake_JWT(self):
+        token = 'Fake_Token'
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_client_without_JWT(self):
+
+        url = reverse('clients-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_add_client_as_management(self):
+        url = reverse('login')
+        data = {'username': 'StaffManagementA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-list')
+        data = {
+            'FirstName': 'JohnD', 
+            'LastName': 'SmithD',
+            'Email': 'jsd@mail.com',
+            'Phone': '+33123456789',
+            'Mobile': '+33623456789',
+            'CompanyName': 'NewClient1',
+            'SalesContactID': 2,
+            'ClientStatutID': 2
+            }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_add_client_as_sales(self):
+        url = reverse('login')
+        data = {'username': 'StaffSalesA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-list')
+        data = {
+            'FirstName': 'JohnD', 
+            'LastName': 'SmithD',
+            'Email': 'jsd@mail.com',
+            'Phone': '+33123456789',
+            'Mobile': '+33623456789',
+            'CompanyName': 'NewClient2',
+            'SalesContactID': 2,
+            'ClientStatutID': 2
+            }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+    def test_add_client_as_support(self):
+        url = reverse('login')
+        data = {'username': 'StaffSupportA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-list')
+        data = {
+            'FirstName': 'JohnD', 
+            'LastName': 'SmithD',
+            'Email': 'jsd@mail.com',
+            'Phone': '+33123456789',
+            'Mobile': '+33623456789',
+            'CompanyName': 'NewClient3',
+            'SalesContactID': 2,
+            'ClientStatutID': 2
+            }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_add_client_with_fake_JWT(self):
+        token = 'Fake_Token'
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-list')
+        data = {
+            'FirstName': 'JohnD', 
+            'LastName': 'SmithD',
+            'Email': 'jsd@mail.com',
+            'Phone': '+33123456789',
+            'Mobile': '+33623456789',
+            'CompanyName': 'NewClient4',
+            'SalesContactID': 2,
+            'ClientStatutID': 2
+            }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_add_client_without_JWT(self):
+        url = reverse('clients-list')
+        data = {
+            'FirstName': 'JohnD', 
+            'LastName': 'SmithD',
+            'Email': 'jsd@mail.com',
+            'Phone': '+33123456789',
+            'Mobile': '+33623456789',
+            'CompanyName': 'NewClient5',
+            'SalesContactID': 2,
+            'ClientStatutID': 2
+            }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_modify_client_as_sales(self):
+        url = reverse('login')
+        data = {'username': 'StaffSalesB', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-detail', kwargs = {'pk': 1})
+        data = {'CompanyName': 'ModifCompanyName'}
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_modify_client_as_support(self):
+        url = reverse('login')
+        data = {'username': 'StaffSupportA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-detail', kwargs = {'pk': 1})
+        data = {'CompanyName': 'ModifCompanyName'}
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_modify_client_with_fake_JWT(self):
+        token = 'Fake_Token'
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-detail', kwargs = {'pk': 1})
+        data = {'CompanyName': 'ModifCompanyName'}
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_modify_client_without_JWT(self):
+        url = reverse('clients-detail', kwargs = {'pk': 1})
+        data = {'CompanyName': 'ModifCompanyName'}
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_modify_client_as_sales_contact(self):
+        url = reverse('login')
+        data = {'username': 'StaffSalesA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-detail', kwargs = {'pk': 1})
+        data = {'CompanyName': 'ModifCompanyName'}
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["CompanyName"], "ModifCompanyName")
+
+    def test_modify_client_as_management(self):
+        url = reverse('login')
+        data = {'username': 'StaffManagementA', 'password': 'password'}
+        response = self.client.post(url, data, format='json')
+        token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + token)
+
+        url = reverse('clients-detail', kwargs = {'pk': 1})
+        data = {'CompanyName': 'ModifCompanyName2'}
+        response = self.client.patch(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["CompanyName"], "ModifCompanyName2")
--- client/views.py
+++ client/views.py
@@ -9,9 +9,57 @@ from rest_framework import permissions
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class NotAllowed(permissions.BasePermission):
+    """Permission that denies all users."""
+
+    message = "This operation is not allowed."
+
+    def has_permission(self, request, view):
+        """Check the object permission."""
+        return False
+
+class IsSale(permissions.BasePermission):
+    """Permission that denies all users."""
+
+    message = "You must be part of sales team for this operation."
+
+    def has_permission(self, request, view):
+        """Check the object permission."""
+        return request.user.StaffProfileID.Name == "Vente"
+
+class IsSalesContactOrManagement(permissions.BasePermission):
+    """Permission checking if user is a project contributor or author."""
+
+    message = "You must be the sales contact of this project."
+
+    def has_permission(self, request, view):
+        """Check the object permission."""
+        profile_name = request.user.StaffProfileID.Name
+        return (profile_name == "Vente") or (profile_name == "Gestion")
+
+    def has_object_permission(self, request, view, obj):
+        """Check the object permission."""
+        #breakpoint()
+        if request.user.StaffProfileID.Name == "Vente":
+            return request.user == obj.SalesContactID
+
+        return True
 
 class ClientModelViewSet(viewsets.ModelViewSet):
     """Client viewset."""
 
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
+
+    def get_permissions(self):
+        """Instantiate and returns the list of permissions that this view requires."""
+        if self.action in ["list", 'retrieve']:
+            permission_classes = [IsAuthenticated]
+        elif self.action in ["create"]:
+            permission_classes = [IsAuthenticated, IsSale]
+        elif self.action in ["partial_update"]:
+            permission_classes = [IsAuthenticated, IsSalesContactOrManagement]
+        else:
+            permission_classes = [NotAllowed]
+
+        return [permission() for permission in permission_classes]
